d1497d1d194cf9dff9b1dab5a4d0e23184799348
15th August-Bl00DYD4Y <ea_pain@yahoo.com>
2019-06-29 22:15:35 +0000

    Update ai.py

--- ai/ai.py
+++ ai/ai.py
@@ -96,7 +96,6 @@ we need to get the players voice then convert them into words
 
 '''
 SOURCES & TUTS:
-https://github.com/ArtyZiff35/3D_Reconstruction_From_Stereo_Images
 https://ai.stackexchange.com/questions/7721/how-does-lstm-in-deep-reinforcement-learning-differ-from-experience-replay
 https://colab.research.google.com/drive/1p51HFDExl7XagWfSQE5leDQwBC6I_e3D
 https://colab.research.google.com/drive/14I-31WuynLg1B0RQHWwwRgBmqTlDgkwV
