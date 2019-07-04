22965ee9d2d34fcfc873a20a670865ca38124ae1
15th August-Bl00DYD4Y <ea_pain@yahoo.com>
2019-07-04 11:34:00 +0000

    Update ai.py

--- ai/ai.py
+++ ai/ai.py
@@ -137,6 +137,7 @@ https://blog.algorithmia.com/introduction-natural-language-processing-nlp/
 https://searchbusinessanalytics.techtarget.com/definition/natural-language-processing-NLP
 https://becominghuman.ai/a-simple-introduction-to-natural-language-processing-ea66a1747b32
 https://www.kdnuggets.com/2018/03/5-things-reinforcement-learning.html
+https://mc.ai/understanding-input-and-output-shape-in-lstm-keras/
 https://medium.com/analytics-vidhya/understanding-genetic-algorithms-in-the-artificial-intelligence-spectrum-7021b7cc25e7
 https://ai.stackexchange.com/questions/7721/how-does-lstm-in-deep-reinforcement-learning-differ-from-experience-replay
 https://colab.research.google.com/drive/1p51HFDExl7XagWfSQE5leDQwBC6I_e3D
