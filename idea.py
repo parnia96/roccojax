665bd9a8b9c9218740cc38d398fec7cfd930ad18
15th August-Bl00DYD4Y <ea_pain@yahoo.com>
2019-07-12 09:53:46 +0000

    Update idea.py

--- idea.py
+++ idea.py
@@ -99,6 +99,7 @@ we need to get the players voice then convert them into words
 
 '''
 SOURCES & TUTS:
+https://github.com/openai/baselines
 https://towardsdatascience.com/paper-repro-deep-neuroevolution-756871e00a66
 http://aqibsaeed.github.io/2017-08-11-genetic-algorithm-for-optimizing-rnn/
 https://adventuresinmachinelearning.com/gensim-word2vec-tutorial/
