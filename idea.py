c12c908b487bb682cd87ac69010a81abb6439496
15th August-Bl00DYD4Y <ea_pain@yahoo.com>
2019-08-08 17:44:32 +0430

    Update idea.py

--- idea.py
+++ idea.py
@@ -98,7 +98,8 @@ we need to get the players voice then convert them into words
 
 
 '''
-SOURCES & TUTS:
+SOURCES:
+https://www.youtube.com/channel/UCWN3xxRkmTPmbKwht9FuE5A
 https://appliedmachinelearning.blog/2019/03/04/state-of-the-art-text-classification-using-bert-model-predict-the-happiness-hackerearth-challenge/
 https://www.kaggle.com/httpwwwfszyc/bert-keras-with-warmup-and-excluding-wd-parameters
 https://www.youtube.com/watch?v=ycXWAtm22-w
